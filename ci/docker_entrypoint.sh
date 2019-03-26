3d41624ce9d2b198463d0d10c90280b5d2f927a9
vijay.jatam <vijay.jatam@contractor.chanzuckerberg.com>
2019-03-26 21:57:26 +0530

    Added Docker based configuration

--- ci/docker_entrypoint.sh
+++ ci/docker_entrypoint.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 
-CLASS_NAME="com.java.example.MyFirstExample"
-echo "Class Name: ${CLASS_NAME}"
+CLASS_NAME1="com.java.example.MyFirstExample"
+CLASS_NAME2="com.java.example.MySecondExample"
+CLASS_NAME3="com.java.example.App"
+echo "Class Name1: ${CLASS_NAME1}"
+echo "Class Name2: ${CLASS_NAME2}"
+echo "Class Name3: ${CLASS_NAME3}"
+
+
 echo "JAR_FILE: ${JAR_FILE}"
 
 cd /opt
 ls
 
-java -cp /opt/${JAR_FILE} ${CLASS_NAME}
+java -cp /opt/${JAR_FILE} ${CLASS_NAME1}
+java -cp /opt/${JAR_FILE} ${CLASS_NAME2}
+java -cp /opt/${JAR_FILE} ${CLASS_NAME3}
